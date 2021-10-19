0dffd2aea65c838a3abba1b62c7494620c9f155c
4G3NT <zoolduel@gmail.com>
2021-10-18 23:45:33 -0700

    Improve variable names & source code.

--- main.go
+++ main.go
@@ -12,55 +12,64 @@ import (
 	"github.com/fatih/color"
 )
 
-func genRandNum(num int) int {
-	seedNum := time.Now().UnixNano()
-	rand.Seed(seedNum)
-	return rand.Intn(num) + 1
+func randNum(rng int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(rng) + 1
 }
 
-func input(prompt string, rdr *bufio.Reader) string {
-	fmt.Print(prompt)
+func input(prmpt string, rdr *bufio.Reader) string {
+	fmt.Print(prmpt)
 	input, _ := rdr.ReadString('\n')
 	return strings.TrimSpace(input)
 }
 
 func main() {
+	red := color.New(color.FgRed).FprintfFunc()
+
 	reader := bufio.NewReader(os.Stdin)
-	argument := input("How much range would you like to put?\n\nE.g. 100 will do 1-100: ", reader)
-	arg, err := strconv.Atoi(argument)
+askForRange:
+	rngInput := input("How much range would you like to put?\n\ne.g. 100 will do 1 -> 100: ", reader)
+
+	arg, err := strconv.Atoi(rngInput)
 
 	if err != nil {
-		color.Red("Error, Please enter a number instead of letter(s)")
+		red(os.Stderr, "error: \"%v\" is not a number.\n", rngInput)
+		goto askForRange
 	}
 
-	secret := genRandNum(arg)
-	fmt.Printf("I've randomly generated a random number 1-%v.\n", arg)
-	attempsStr := input("How many attempts would you like?: ", reader)
+	rand := randNum(arg)
+	fmt.Printf("I've randomly generated a random number 1 -> %v.\n", arg)
+askForAttempts:
+	attempts := input("How many attempts would you like?: ", reader)
 
-	attemptsConverted, err := strconv.Atoi(attempsStr)
+	attemptsConverted, err := strconv.Atoi(attempts)
 
 	if err != nil {
-		color.Red("Error, Please enter a number instead of letter(s)")
+		red(os.Stderr, "error: \"%v\" is not a number.\n", attempts)
+		goto askForAttempts
 	}
 
 	for tries := 0; tries < attemptsConverted; tries++ {
 		fmt.Printf("You have %v guesses left.\n", attemptsConverted-tries)
 
-		inp := input("Enter your guess: ", reader)
+		gss := input("Enter your guess: ", reader)
 
-		guess, err := strconv.Atoi(inp)
+		guess, err := strconv.Atoi(gss)
 
 		if err != nil {
+			tries--
 			color.Red("Error, Please enter a number instead of letter(s)")
+			continue
 		}
 
-		if guess < secret {
+		if guess < rand {
 			color.Yellow("Oops. Your guess was too LOW.")
-		} else if guess > secret {
+		} else if guess > rand {
 			color.Yellow("Oops. Your guess was too HIGH.")
 		} else {
-			color.Green("\n\nYou guessed the number!, The number was %v.\n", secret)
-			break
+			color.Green("\n\nYou guessed the number!, The number was %v.\n", rand)
+			os.Exit(0)
 		}
 	}
+	fmt.Printf("Sadly you did't guess the number :(, The number was %v\n", rand)
 }
